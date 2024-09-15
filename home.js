3c2938bcc3e6377485414f945b029fa764f64ef8
KJayaraju <146174744+KJayaraju@users.noreply.github.com>
2024-09-15 13:53:51 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,11 @@
+let a=document.getElementById("button");
+function para(){
+    const node = document.createElement("li");
+    node.textContent="hello"
+    document.getElementById("button").appendChild(node);
+}
+function par(){
+    const f=document.getElementById("form19");
+    const r=f.value;
+    console.log(r);
+}
